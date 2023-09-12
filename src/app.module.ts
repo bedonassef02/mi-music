5e955e1be5f1e639ebbadbedaa03249c3d6b6aef
Bedo <bedonassef71@gmail.com>
2023-09-12 23:03:12 +0300

    connect to mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongModule } from './song/song.module';
 import { ArtistModule } from './artist/artist.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [SongModule, ArtistModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }),DatabaseModule,SongModule, ArtistModule],
   controllers: [AppController],
   providers: [AppService],
 })
